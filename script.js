23578d1d11d9db97793e9d9d3527c6840dfa58cd
Katya <katya.mikhaylova.98@list.ru>
2022-11-24 19:35:22 +0300

    fix: responsive design

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const burger = document?.querySelector('[data-burger]')
 const nav = document?.querySelector('[data-nav]')
+const navItems = nav?.querySelectorAll('a')
+const body = document.body
 
 burger?.addEventListener('click', () => {
+  body.classList.toggle('stop-scroll')
+  burger?.classList.toggle('burger--active')
   nav.classList.toggle('nav--visible')
+})
+
+navItems.forEach(el => {
+  el.addEventListener('click', () => {
+    body.classList.remove('stop-scroll')
+    burger?.classList.remove('burger--active')
+    nav.classList.remove('nav--visible')
+  })
 })
